af85cb04bd9a11b43c7747f7ffc6460991372c93
CatwisBot <whisnuaryanugraha04@gmail.com>
2026-08-03 08:42:04 +0700

    fix: add eslint disable comment for loadData in useEffect to prevent warning

--- app/admin/pendaftaran/page.tsx
+++ app/admin/pendaftaran/page.tsx
@@ -69,6 +69,7 @@ export default function AdminPendaftaranPage() {
   }, []);
 
   useEffect(() => {
+    // eslint-disable-next-line react-hooks/set-state-in-effect
     loadData();
   }, [loadData]);
 
