a60c59a1a39e00424eb724b4c6264faca1cdd745
CatwisBot <whisnuaryanugraha04@gmail.com>
2026-08-03 08:32:19 +0700

    Update next.config.ts to configure experimental server actions and image qualities

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,14 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  experimental: {
+    serverActions: {
+      bodySizeLimit: "10mb",
+    },
+  },
+  images: {
+    qualities: [75, 100],
+  },
 };
 
 export default nextConfig;
